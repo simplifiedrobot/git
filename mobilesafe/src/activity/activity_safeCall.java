485c629933d82a5d05fc6d731369bc54311e4afc
simplifiedrobot <ws163163@yeah.net>
2015-12-16 22:32:22 +0800

    黑名单分页显示

--- mobilesafe/src/activity/activity_safeCall.java
+++ mobilesafe/src/activity/activity_safeCall.java
@@ -1,10 +1,8 @@
 package activity;
 
-
-
-
 import java.util.List;
 
+import utils.mToast;
 import Adapter.MyBaseAdapter;
 import android.app.Activity;
 import android.content.Context;
@@ -12,11 +10,13 @@ import android.os.Bundle;
 import android.os.Handler;
 import android.os.Message;
 import android.os.SystemClock;
+import android.text.TextUtils;
 import android.view.View;
+import android.view.View.OnClickListener;
 import android.view.ViewGroup;
+import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
-import android.widget.ListAdapter;
 import android.widget.ListView;
 import android.widget.TextView;
 import bean.blackNumberInfo;
@@ -27,11 +27,14 @@ import dao.BlackNumberDao;
 
 public class activity_safeCall extends Activity {
 
-	  private BlackNumberDao numberDao;
+	private BlackNumberDao numberDao;
 	private List<blackNumberInfo> AllNumber;
 	private View view;
 	private ListView list_view;
 	private LinearLayout l_pb;
+	private int currentPage;
+	private MyAdapter adapter;
+
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		// TODO Auto-generated method stub
@@ -39,75 +42,153 @@ public class activity_safeCall extends Activity {
 		setContentView(R.layout.activity_safecall);
 		InitDate();
 		InitUI();
+		currentPage = 1;
+
 		list_view = (ListView) findViewById(R.id.list_view);
 	}
-  	Handler handler=new Handler(){
-  		public void handleMessage(Message msg) {
-  			super.handleMessage(msg);
-  			System.out.println("接受到了消息");
-  			l_pb.setVisibility(View.INVISIBLE);
-  			ListAdapter adapter=new MyAdapter(activity_safeCall.this,AllNumber);
-  			list_view.setAdapter(adapter);
-  		}
-  	};
-	public void InitDate(){
+
+	Handler handler = new Handler() {
+
+		public void handleMessage(Message msg) {
+			super.handleMessage(msg);
+			System.out.println("接受到了消息");
+			TextView tv_pageNum = (TextView) findViewById(R.id.tv_pageNum);
+			tv_pageNum.setText(currentPage + "/" + totalPage);
+			l_pb.setVisibility(View.INVISIBLE);
+			adapter = new MyAdapter(activity_safeCall.this, AllNumber);
+			list_view.setAdapter(adapter);
+		}
+	};
+	private int pageSize;
+	private int totalPage;
+
+	public void InitDate() {
 		numberDao = new BlackNumberDao(this);
-		new Thread(){
+		pageSize = 15;
+		new Thread() {
 			public void run() {
-				AllNumber =numberDao.findAllNumber();
-				SystemClock.sleep(500);
+				AllNumber = numberDao.findParNumber(pageSize, currentPage);
+				SystemClock.sleep(200);
 				handler.sendEmptyMessage(0);
 			};
 		}.start();
 	}
+
+	public void prePage(View v) {
+		if (currentPage <= 1) {
+			mToast.show(this, "已经是第一页");
+			return;
+		} else {
+			currentPage = currentPage - 1;
+		}
+		InitDate();
+	}
+
+	public void nextPage(View v) {
+
+		if (currentPage >= (totalPage)) {
+			mToast.show(this, "已经是最后一页");
+			return;
+		} else {
+			currentPage = currentPage + 1;
+		}
+		InitDate();
+	}
+
+	public void jumpPage(View v) {
+		EditText et_pageNum = (EditText) findViewById(R.id.et_pageNum);
+		String page = et_pageNum.getText().toString();
+		if (!TextUtils.isEmpty(page)) {
+			currentPage = Integer.parseInt(page);
+			if (currentPage <= 1 || currentPage >= totalPage) {
+				mToast.show(this, "请输入正确的页码");
+				return;
+			} else {
+				InitDate();
+			}
+		}
+
+	}
+
 	private void InitUI() {
+		int getTotalPage = numberDao.getTotalPage();
+		if (getTotalPage % pageSize == 0) {
+			totalPage = getTotalPage / pageSize;
+		} else {
+			totalPage = getTotalPage / pageSize + 1;
+		}
 		l_pb = (LinearLayout) findViewById(R.id.pb);
 		l_pb.setVisibility(View.VISIBLE);
 	}
-	class MyAdapter extends MyBaseAdapter<blackNumberInfo>{
+
+	class MyAdapter extends MyBaseAdapter<blackNumberInfo> {
 		public MyAdapter() {
 			super();
 			// TODO Auto-generated constructor stub
 		}
+
 		public MyAdapter(Context mcontex, List<blackNumberInfo> list) {
 			super(mcontex, list);
 			// TODO Auto-generated constructor stub
 		}
+
 		private ViewHolder holder;
+
 		@Override
 		public View getView(int position, View convertView, ViewGroup parent) {
-		System.out.println("进入适配器");
-			if(convertView==null){
-				convertView = View.inflate(activity_safeCall.this, R.layout.listview_safecall_layout, null);
+			System.out.println("进入适配器");
+			if (convertView == null) {
+				convertView = View.inflate(activity_safeCall.this,
+						R.layout.listview_safecall_layout, null);
 				holder = new ViewHolder();
-				holder.tv_blacknum=(TextView) convertView.findViewById(R.id.tv_blacknum);
-				holder.tv_mode=(TextView) convertView.findViewById(R.id.tv_mode);
-				holder.iv_delete=(ImageView) convertView.findViewById(R.id.iv_delete);
-			    convertView.setTag(holder);
-			}else{
-				holder=(ViewHolder) convertView.getTag();
+				holder.tv_blacknum = (TextView) convertView
+						.findViewById(R.id.tv_blacknum);
+				holder.tv_mode = (TextView) convertView
+						.findViewById(R.id.tv_mode);
+				holder.iv_delete = (ImageView) convertView
+						.findViewById(R.id.iv_delete);
+				convertView.setTag(holder);
+			} else {
+				holder = (ViewHolder) convertView.getTag();
 			}
 			System.out.println("填充成功");
-			 holder.tv_blacknum.setText(list.get(position).getNumber());
-	            String mode = list.get(position).getMode();
-	         switch (Integer.parseInt(mode)){
+			holder.tv_blacknum.setText(list.get(position).getNumber());
+			String mode = list.get(position).getMode();
+			switch (Integer.parseInt(mode)) {
 			case 1:
-				 holder.tv_mode.setText("来电拦截+短信");
+				holder.tv_mode.setText("来电拦截+短信");
 				break;
 			case 2:
-				 holder.tv_mode.setText("电话拦截");
+				holder.tv_mode.setText("电话拦截");
 				break;
 			case 3:
-				 holder.tv_mode.setText("短信拦截");
+				holder.tv_mode.setText("短信拦截");
 				break;
 			default:
 				break;
 			}
-	            System.out.println("填充成功，返回view");
-		return convertView;
+			System.out.println("填充成功，返回view");
+			final blackNumberInfo blackNum = list.get(position);
+			holder.iv_delete.setOnClickListener(new OnClickListener() {
+				@Override
+				public void onClick(View v) {
+					String num = blackNum.getNumber();
+					Boolean b = numberDao.delete(num);
+					if (b) {
+						mToast.show(activity_safeCall.this, "删除成功");
+						list.remove(blackNum);
+						// 刷新界面
+						adapter.notifyDataSetChanged();
+					} else {
+						mToast.show(activity_safeCall.this, "删除失败");
+					}
+				}
+			});
+			return convertView;
 		}
 	}
-	static class  ViewHolder{
+
+	static class ViewHolder {
 		TextView tv_blacknum;
 		TextView tv_mode;
 		ImageView iv_delete;
--- mobilesafe/src/dao/BlackNumberDao.java
+++ mobilesafe/src/dao/BlackNumberDao.java
@@ -12,106 +12,130 @@ import bean.blackNumberInfo;
 public class BlackNumberDao {
 
 	public BlackNumberOpenHelper helper;
-	public BlackNumberDao(Context context){
-		helper=new BlackNumberOpenHelper(context);
+
+	public BlackNumberDao(Context context) {
+		helper = new BlackNumberOpenHelper(context);
 	}
-	//增加号码和拦截模式
-	public boolean add(String number,String mode){
+
+	// 增加号码和拦截模式
+	public boolean add(String number, String mode) {
 		SQLiteDatabase db = helper.getWritableDatabase();
-		ContentValues values=new ContentValues();
+		ContentValues values = new ContentValues();
 		values.put("number", number);
-		values.put("mode",mode);
-	      long i=   db.insert("blacknumber", null, values);
-		if(i==-1){
+		values.put("mode", mode);
+		long i = db.insert("blacknumber", null, values);
+		if (i == -1) {
 			return false;
-		}else{
+		} else {
 			return true;
 		}
 	}
-	//按号码删除
-	public boolean delete(String number){
+
+	// 按号码删除
+	public boolean delete(String number) {
 		SQLiteDatabase db = helper.getWritableDatabase();
-	     int i=db.delete("blacknumble", "number=?", new String []{number} );
-			if(i==0){
-				return false;
-			}else{
-				return true;
-			}
-			}
-	//通过号码修改拦截模式
-	public boolean changNumberMode(String number,String mode){
-		SQLiteDatabase db = helper.getWritableDatabase();
-		ContentValues values=new ContentValues();
-		values.put("mode",mode);
-	   int i=	db.update("blacknumber", values, "number=?", new String[]{number});
-		if(i==0){
+		int i = db.delete("blacknumble", "number=?", new String[] { number });
+		if (i == 0) {
 			return false;
-		}else{
+		} else {
 			return true;
 		}
+	}
+
+	// 通过号码修改拦截模式
+	public boolean changNumberMode(String number, String mode) {
+		SQLiteDatabase db = helper.getWritableDatabase();
+		ContentValues values = new ContentValues();
+		values.put("mode", mode);
+		int i = db.update("blacknumber", values, "number=?",
+				new String[] { number });
+		if (i == 0) {
+			return false;
+		} else {
+			return true;
 		}
-	//返回一个黑名单号码的拦截模式
-	public String findNumber(String number){
-		
-		SQLiteDatabase db = helper.getWritableDatabase();
-		String mode="";
-     	Cursor cursor=	db.query("blacknumber", new String[ ]{"mode"}, "number=?", new String[]{number}, null, null, null);
-	   if(cursor.moveToNext()){
-			mode=cursor.getString(0);
-	   }
-	   cursor.close();
-	   db.close();
-	   return mode;
 	}
-	//查询所有的黑名单
-	public List<blackNumberInfo> findAllNumber(){
+
+	// 返回一个黑名单号码的拦截模式
+	public String findNumber(String number) {
+
 		SQLiteDatabase db = helper.getWritableDatabase();
-		List <blackNumberInfo> numberList=new ArrayList<blackNumberInfo>();
-		Cursor cursor=db.query("blacknumber", new String[]{"number","mode"}, null, null, null, null, null);
-	    while(cursor.moveToNext()){
-	    	blackNumberInfo numberInfo = new blackNumberInfo();
-	    	numberInfo.setNumber(cursor.getString(0));
-	    	numberInfo.setMode(cursor.getString(1));
-	    	numberList.add(numberInfo);
-	    }
-		
-	   cursor.close();
-	   db.close();
-	   return numberList;
+		String mode = "";
+		Cursor cursor = db.query("blacknumber", new String[] { "mode" },
+				"number=?", new String[] { number }, null, null, null);
+		if (cursor.moveToNext()) {
+			mode = cursor.getString(0);
+		}
+		cursor.close();
+		db.close();
+		return mode;
 	}
-	
-	//分页加载数据
-	public List<blackNumberInfo> findParNumber(int PageSize,int page){
+
+	// 查询所有的黑名单
+	public List<blackNumberInfo> findAllNumber() {
 		SQLiteDatabase db = helper.getWritableDatabase();
-		List <blackNumberInfo> numberList=new ArrayList<blackNumberInfo>();
-		Cursor cursor=db.rawQuery("select number,mode from blacknumber limit ? offset ?",
-				new String[]{ String.valueOf(PageSize),String.valueOf(page*PageSize)});
-	    while(cursor.moveToNext()){
-	    	blackNumberInfo numberInfo = new blackNumberInfo();
-	    	numberInfo.setNumber(cursor.getString(0));
-	    	numberInfo.setMode(cursor.getString(1));
-	    	numberList.add(numberInfo);
-	    }
-	   cursor.close();
-	   db.close();
-	   return numberList;
+		List<blackNumberInfo> numberList = new ArrayList<blackNumberInfo>();
+		Cursor cursor = db
+				.query("blacknumber", new String[] { "number", "mode" }, null,
+						null, null, null, null);
+		while (cursor.moveToNext()) {
+			blackNumberInfo numberInfo = new blackNumberInfo();
+			numberInfo.setNumber(cursor.getString(0));
+			numberInfo.setMode(cursor.getString(1));
+			numberList.add(numberInfo);
+		}
+
+		cursor.close();
+		db.close();
+		return numberList;
 	}
-	
-	//分批加载数据
-	public List<blackNumberInfo> findParNumber2(int startIndex,int maxCount){
+
+	// 分页加载数据
+	public List<blackNumberInfo> findParNumber(int PageSize, int page) {
 		SQLiteDatabase db = helper.getWritableDatabase();
-		List <blackNumberInfo> numberList=new ArrayList<blackNumberInfo>();
-		Cursor cursor=db.rawQuery("select number,mode from blacknumber limit ? offset ?",
-				new String[]{ String.valueOf(maxCount),String.valueOf(startIndex)});
-	    while(cursor.moveToNext()){
-	    	blackNumberInfo numberInfo = new blackNumberInfo();
-	    	numberInfo.setNumber(cursor.getString(0));
-	    	numberInfo.setMode(cursor.getString(1));
-	    	numberList.add(numberInfo);
-	    }
-	   cursor.close();
-	   db.close();
-	   return numberList;
+		List<blackNumberInfo> numberList = new ArrayList<blackNumberInfo>();
+		Cursor cursor = db.rawQuery(
+				"select number,mode from blacknumber limit ? offset ?",
+				new String[] { String.valueOf(PageSize),
+						String.valueOf(page * (PageSize - 1)) });
+		while (cursor.moveToNext()) {
+			blackNumberInfo numberInfo = new blackNumberInfo();
+			numberInfo.setNumber(cursor.getString(0));
+			numberInfo.setMode(cursor.getString(1));
+			numberList.add(numberInfo);
+		}
+		cursor.close();
+		db.close();
+		return numberList;
 	}
-	
+
+	// 分批加载数据
+	public List<blackNumberInfo> findParNumber2(int startIndex, int maxCount) {
+		SQLiteDatabase db = helper.getWritableDatabase();
+		List<blackNumberInfo> numberList = new ArrayList<blackNumberInfo>();
+		Cursor cursor = db.rawQuery(
+				"select number,mode from blacknumber limit ? offset ?",
+				new String[] { String.valueOf(maxCount),
+						String.valueOf(startIndex) });
+		while (cursor.moveToNext()) {
+			blackNumberInfo numberInfo = new blackNumberInfo();
+			numberInfo.setNumber(cursor.getString(0));
+			numberInfo.setMode(cursor.getString(1));
+			numberList.add(numberInfo);
+		}
+		cursor.close();
+		db.close();
+		return numberList;
+	}
+
+	public int getTotalPage() {
+		SQLiteDatabase db = helper.getReadableDatabase();
+		Cursor cursor = db.rawQuery("select count(*) from blacknumber", null);
+		cursor.moveToNext();
+		int total = cursor.getInt(0);
+		cursor.close();
+		db.close();
+		return total;
+	}
+
 }
